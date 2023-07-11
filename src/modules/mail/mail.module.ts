b28c0732d220f27ea7fa398c3ab4219e7c3e4654
tuannt02 <20522122@gm.uit.edu.vn>
2023-07-11 16:52:58 +0700

    fix configure

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,26 +1,31 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        service: 'gmail',
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.USER_GMAIL,
-          pass: process.env.PASS_GMAIL,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          service: 'gmail',
+          port: 587,
+          secure: false,
+          auth: {
+            user: configService.get<string>('USER_GMAIL'),
+            pass: configService.get<string>('PASS_GMAIL'),
+          },
         },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
+        defaults: {
+          from: '"No Reply" <noreply@example.com>',
+        },
+      }),
     }),
   ],
-  providers: [MailService],
+  providers: [MailService, ConfigService],
   exports: [MailService],
 })
 export class MailModule {}
