ead3d73acb37b9381d8a9d3e4f24cdf3eae4c8d6
tuannt02 <20522122@gm.uit.edu.vn>
2023-07-18 10:47:49 +0700

    add unit test for api verify token

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -26,40 +26,115 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('Email exist', async () => {
-    const params: SignupDto = {
-      email: '20522212@gm.uit.edu.vn',
-      password: 'Tuan2002@',
-    };
+  describe('Sign up', () => {
+    it('Email exist', async () => {
+      const params: SignupDto = {
+        email: '20522212@gm.uit.edu.vn',
+        password: 'Tuan2002@',
+      };
 
-    jest
-      .spyOn(service['userRepo'], 'getUserByEmail')
-      .mockRejectedValue(new CustomErrorException(ERRORS.EmailExisted));
+      jest
+        .spyOn(service['userRepo'], 'getUserByEmail')
+        .mockRejectedValue(new CustomErrorException(ERRORS.EmailExisted));
 
-    try {
-      await service.signup(params);
-    } catch (err) {
-      expect(err.code).toEqual(ERRORS.EmailExisted.code);
-      expect(err.statusCode).toEqual(ERRORS.EmailExisted.statusCode);
-      expect(err.message).toEqual(ERRORS.EmailExisted.message);
-    }
+      try {
+        await service.signup(params);
+      } catch (err) {
+        expect(err.code).toEqual(ERRORS.EmailExisted.code);
+        expect(err.statusCode).toEqual(ERRORS.EmailExisted.statusCode);
+        expect(err.message).toEqual(ERRORS.EmailExisted.message);
+      }
+    });
+
+    it('User sign up successfully', async () => {
+      const params: SignupDto = {
+        email: '20522212@gm.uit.edu.vn',
+        password: 'Tuan2002@',
+      };
+
+      jest
+        .spyOn(service['userRepo'], 'getUserByEmail')
+        .mockResolvedValue(undefined);
+
+      jest.spyOn(service['userRepo'], 'createUser').mockResolvedValue();
+
+      const res = await service.signup(params);
+      expect(res).toEqual({
+        message: 'An Email sent to your account please verify',
+      });
+    });
   });
 
-  it('User sign up successfully', async () => {
-    const params: SignupDto = {
-      email: '20522212@gm.uit.edu.vn',
-      password: 'Tuan2002@',
-    };
+  describe('Verify token', () => {
+    it('Email not exist', async () => {
+      const params: { email: string; token: string } = {
+        email: '20522122@gm.uit.edu.vn',
+        token:
+          '06d7588460b728a5a6a062b614f716d3ff5795b8ee522a115b7438fc84d4651b',
+      };
 
-    jest
-      .spyOn(service['userRepo'], 'getUserByEmail')
-      .mockResolvedValue(undefined);
+      jest
+        .spyOn(service['userRepo'], 'getUserByEmail')
+        .mockResolvedValue(undefined);
 
-    jest.spyOn(service['userRepo'], 'createUser').mockResolvedValue();
+      try {
+        await service.verifyToken(params.email, params.token);
+      } catch (err) {
+        expect(err.code).toEqual(ERRORS.InvalidLink.code);
+        expect(err.statusCode).toEqual(ERRORS.InvalidLink.statusCode);
+        expect(err.message).toEqual(ERRORS.InvalidLink.message);
+      }
+    });
 
-    const res = await service.signup(params);
-    expect(res).toEqual({
-      message: 'An Email sent to your account please verify',
+    it('Verify token not match', async () => {
+      const params: { email: string; token: string } = {
+        email: '20522122@gm.uit.edu.vn',
+        token:
+          '06d7588460b728a5a6a062b614f716d3ff5795b8ee522a115b7438fc84d4651b',
+      };
+
+      jest.spyOn(service['userRepo'], 'getUserByEmail').mockResolvedValue({
+        email: '20522122@gm.uit.edu.vn',
+        isMailActive: false, // Accepted all true or false
+        password:
+          '$2b$10$LA3S1FYXxRiBwHrrpZNdHumOEp0PVsPY43MdW9hpnXRVPLyaoagb.',
+        verifyToken:
+          '07d7588460b728a5a6a062b614f716d3ff5795b8ee522a115b7438fc84d4651b', // Can be empty string
+        createdAt: 1689343792,
+        updatedAt: 1689343792,
+      });
+
+      try {
+        await service.verifyToken(params.email, params.token);
+      } catch (err) {
+        expect(err.code).toEqual(ERRORS.InvalidLink.code);
+        expect(err.statusCode).toEqual(ERRORS.InvalidLink.statusCode);
+        expect(err.message).toEqual(ERRORS.InvalidLink.message);
+      }
+    });
+
+    it('Verify token successfully', async () => {
+      const params: { email: string; token: string } = {
+        email: '20522122@gm.uit.edu.vn',
+        token:
+          '06d7588460b728a5a6a062b614f716d3ff5795b8ee522a115b7438fc84d4651b',
+      };
+
+      jest.spyOn(service['userRepo'], 'getUserByEmail').mockResolvedValue({
+        email: '20522122@gm.uit.edu.vn',
+        isMailActive: false, // Accepted all true or false
+        password:
+          '$2b$10$LA3S1FYXxRiBwHrrpZNdHumOEp0PVsPY43MdW9hpnXRVPLyaoagb.',
+        verifyToken:
+          '06d7588460b728a5a6a062b614f716d3ff5795b8ee522a115b7438fc84d4651b', // Can be empty string
+        createdAt: 1689343792,
+        updatedAt: 1689343792,
+      });
+
+      const res = await service.verifyToken(params.email, params.token);
+      expect(res).toEqual({
+        message: 'Email verified successfully',
+      });
     });
   });
 });
